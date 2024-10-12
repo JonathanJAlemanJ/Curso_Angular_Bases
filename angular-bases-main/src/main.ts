44d8682fe064d344a7df929079a05b429348e560
Jonathan Julian Aleman Jimenez <juli30puma59@gmail.com>
2024-10-11 21:37:39 -0600

    si

--- angular-bases-main/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-// Esta linea es para configurar la aplicacion (web) para diferentes plataformas
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
